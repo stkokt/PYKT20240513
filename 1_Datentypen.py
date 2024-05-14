a7ddc73633c089dfaf88ce22154648e5fed97345
stkokt <156156025+stkokt@users.noreply.github.com>
2024-05-14 12:18:25 +0200

    Create 1_Datentypen.py

---
+++ 1_Datentypen.py
@@ -0,0 +1,39 @@
+print("Hello World")
+# builtin Funktionen, 
+
+var="Hallo Welt"
+var1="Hallo"
+var2=5
+var3=25
+print(var.lower())
+
+print('Frank\'s Kittchen')
+print(f'Ich sage {var2*var1}')
+
+var2**=2
+
+# 0000 2^3 2^2 2^1 2^0
+# 0011 = 3
+# 0100 = 4
+# >>= 0010 = 2
+# <<= 1000 = 8
+
+# AND, OR, XOR
+# 0010 XOR
+# 0011
+# 0001
+
+print(2|3)
+
+
+
+
+
+
+
+
+# Maschine ein aus -> Objekt einer Klasse
+# Wasser einfüllen, Kaffee einfüllen -> Wiederholungen, logische Verzweigung, Zustand (einer Variablen), Verändern den Zustand mit Operatoren
+# Menuführung -> Verzweigung
+
+
