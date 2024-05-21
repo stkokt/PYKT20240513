1aa8e3b1f89e49f97f2cff60109a35a71a5092e3
Stefan Koschnik <stefan.koschnik@karrieretutor.de>
2024-05-21 16:37:51 +0200

    Verzweigungen
    Verzweigungen

--- 1_Datentypen.py
+++ 1_Datentypen.py
@@ -1,29 +1,78 @@
-print("Hello World")
-# builtin Funktionen, 
+# Integer und ein bisschen binär
 
-var="Hallo Welt"
-var1="Hallo"
-var2=5
-var3=25
-print(var.lower())
+int_a=10
+int_b=int_a
+print(int_a, int_b)
+int_a=11
+print(int_a, int_b)
+print(int_a & int_b)
+int_a=1
+int_b=2
+print(bin(int_a) +'\n'+bin(int_b))
+print(bin(int_a | int_b))
+print(bin((int_a | int_b) <<2))
 
-print('Frank\'s Kittchen')
-print(f'Ich sage {var2*var1}')
+# Float und Runden
 
-var2**=2
+float_a=1043.791854
+print(round(float_a, 3))
 
-# 0000 2^3 2^2 2^1 2^0
-# 0011 = 3
-# 0100 = 4
-# >>= 0010 = 2
-# <<= 1000 = 8
+float_b=1.9999999999999999
+float_b=1.9999777777777777
+print(int(float_b))
 
-# AND, OR, XOR
-# 0010 XOR
-# 0011
-# 0001
+from math import ceil
+from math import floor
 
-print(2|3)
+print(floor(float_b))
+print(round(float_b,5))
+
+# Listen
+# Listen Methoden: append, extend, copy, count, index, insert, pop, remove, reverse, sort
+# builtin: all()/any()
+
+list_a=[1,2,3,4,5,6,7,8,9,10]
+print(list_a[0:5])
+print(list_a[0:int((len(list_a)/2))])
+list_a.reverse()
+print((list_a))
+print((list_a[::-1]))
+list_b=list_a
+list_b=[20,19,18]
+list_c=[17,16,15]
+list_b.append(list_a)
+print((list_b))
+list_b.extend(list_c)
+print((list_b))
+list_c.sort(reverse=1)
+print((list_c))
+list_d=list_c.copy()
+#list_d=list_c
+print(list_d)
+list_d[0]=100
+print(id(list_c), id(list_d))
+print(list_d.index(16))
+list_e=[1,2,3,1,5,2,4,1,0]
+print(list_e.count(1))
+list_f=list_a+list_e
+print(set(list_f))
+#all/any
+
+
+# String
+
+str_a="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+print(str_a.rfind("sit"))
+
+print("dolor" in str_a)
+
+# Dictionary
+
+dict_a={"Red":"#ff0000", "Green":"#00ff00", "Blue":"#0000ff"}
+print(dict_a.keys(),
+      dict_a.values(),
+      dict_a.items(),
+      dict_a.get("Red"))
 
 
 
@@ -31,9 +80,5 @@ print(2|3)
 
 
 
-
-# Maschine ein aus -> Objekt einer Klasse
-# Wasser einfüllen, Kaffee einfüllen -> Wiederholungen, logische Verzweigung, Zustand (einer Variablen), Verändern den Zustand mit Operatoren
-# Menuführung -> Verzweigung
 
 
---
+++ 2_Verzweigungen.py
@@ -0,0 +1,57 @@
+﻿# Verzweigungen
+
+""" 
+    Es gibt zwei Arten von Verzweigungen, die 'if, elif, else'- Variante und die 'match case'- Variante.
+    Alles was man mit 'match case' machen kann, kann man auch über 'if, elif, else' machen. Umgekehrt ist das nicht
+    immer so.
+    Grundsätzlich wird bei Verzweigungen der Zustand einer oder mehrerer Variablen ausgewertet (Rückgabewert ist Boolean)
+    und auf Basis der Entscheidung ein damit assoziierter Prozess ausgeführt.
+"""
+
+# Die if, elif, else Variante
+
+# Fall 1: Die zu prüfende Variable kann nur 2 Zustände annehmen.
+
+lichtschalter=False # Licht ist aus
+print(f"Zur Veranschaulichung Konvertierung eines Wahrheitswerts (lichtschalter=={lichtschalter}) in Integer: ", int(lichtschalter))
+
+if lichtschalter==False:
+    print("Licht ist aus.")
+else:   # Also das Gegenteil von Zeile 17, lichtschalter == True
+    print("Licht ist an.")
+
+# Fall 2: Die Prüfvariable kann mehr als zwei Zustände annehmen.
+
+dimmer=70    # Alles zwischen 0 und 100 soll als "Gedimmt" (mit 2 Stufen) ausgewertet werden, 0 ist "Dunkel", 100 ist "Hell"
+
+if dimmer <= 0:
+    print("Dunkel")
+elif dimmer>0 and dimmer <50:
+    print("Gedimmt, eher dunkel")
+elif dimmer>50 and dimmer <100:
+    print("Gedimmt, eher hell")
+else:
+    print("Hell")
+
+# Verkürzende Schreibweisen für if- Bedingungen
+
+"""
+Da auch 0 als False und 1 als True ausgewertet werden, kann jede Variable (z.B. var1) auch mit 'if var1' abgefragt werden.
+
+var1 = 0   # Wenn var1 = irgendein Wert außer 0, dann if var1 == True 
+if var1:
+    print("False)
+
+Bei sequenziellen (iterierbaren) Datentypen ist die Länge entscheidend.
+
+einText = ""  # Variable mit leerem String initialisiert, daher if einText == False
+einText = "einText"   # jetzt if einText == True
+listeA = []  # Variable mit leerer Liste initialisiert, daher if listeA == False
+listeA = [1,2,3]    # jetzt if listeA == True
+
+Die if- Bedingung kann auch mit logischen Operatoren abgefragt werden.
+
+Im Lichtschalter- Bsp. ist das Licht aus, wenn die Variable auf False steht.
+if (lichtschalter) erfragt also, ob lichtschalter == True,
+if (not lichtschalter) erfragt hingegen, ob lichtschalter == False
+""" 
